1bc5128acd95a840615087feb6ab7414db1f2c83
milkyway <hoad3579@gmail.com>
2025-03-04 18:41:58 +0700

    commit-6

--- src/Slices/authSlice.tsx
+++ src/Slices/authSlice.tsx
@@ -22,7 +22,7 @@ export const loginUser = createAsyncThunk(
     async ({ account, password }: { account: string; password: string }, { rejectWithValue }) => {
         try {
             const response = await fetch(
-                `/users/login?account=${account}&password=${password}`,
+                `${API_URL}/users/login?account=${account}&password=${password}`,
                 {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
